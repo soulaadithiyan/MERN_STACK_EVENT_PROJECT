a3265b941e8c2587296bff4d9f2ac63403f7e08f
soulaadithiyan <rsraadithiyan2209@gmail.com>
2025-05-04 14:46:15 +0530

    first commit

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+      At Wedly, we believe that every celebration deserves to be unforgettable. Whether you're planning a dreamy wedding, an elegant engagement, or a grand corporate event, our mission is to turn your vision into reality—effortlessly and beautifully.Built with modern technology and a passion for perfection, Wedly offers a seamless online platform to explore, plan, and manage every detail of your special day. From finding the perfect venue and trusted vendors to curating guest lists and timelines, we provide all-in-one solutions that simplify the event planning experience.With creativity, precision, and heart, Wedly is here to ensure your moments are not just well-planned—but truly magical.
+      </p>
+    </section>
+  );
+};
+
+export default About;
